4d201e3611310c6f01f550aa0d795447dc82d994
Isaac Sokari <isaacsokari@gmail.com>
2020-08-04 12:45:32 +0100

    added sign in form and its components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/signin-signup/signin-signup.component';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './signin-signup.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUpPage;
