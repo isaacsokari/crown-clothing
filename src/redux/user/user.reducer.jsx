f83645e54aefb88b677b16fac8aadd4578d7c310
Isaac Sokari <isaacsokari@gmail.com>
2020-09-16 20:15:42 +0100

    fix: set the initial state of the currentUser to null.

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,6 +1,6 @@
 import { userActionTypes } from './user.types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { currentUser: null };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
