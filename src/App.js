06f3dc7c37c3dedf7ed1f68901c4845fdc13bed5
Isaac Sokari <isaacsokari@gmail.com>
2020-09-17 21:24:59 +0100

    added checkout route, em[pty cart message and checkout link in button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/signin-signup/signin-signup.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -47,6 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
