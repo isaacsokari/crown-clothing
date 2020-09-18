c458b06ddbd56b13681eef5570829405fa297fb3
Isaac Sokari <isaacsokari@gmail.com>
2020-09-18 19:48:01 +0100

    added collection links to collection preview page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title">
+      {title.toUpperCase()}{' '}
+      <CustomButton
+        onClick={() => history.push(`/shop/${title.toLowerCase()}`)}
+        inverted
+      >
+        Go to {title}
+      </CustomButton>
+    </h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
@@ -17,4 +27,4 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
