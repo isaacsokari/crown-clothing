24dec6db38fed7d0c9119e86b6ceb3a9ab09f03b
Isaac Sokari <isaacsokari@gmail.com>
2020-09-23 00:40:41 +0100

    used redux-saga to manage asynchronous code replacing thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,3 @@
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-
 import ShopActionTypes from './shop.types';
 
 export const fetchCollectionsStart = () => ({
@@ -19,19 +14,19 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   payload: errorMessage,
 });
 
-export const fetchCollectionsStartAsync = () => {
-  return (dispatch) => {
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
+// export const fetchCollectionsStartAsync = () => {
+//   return (dispatch) => {
+//     const collectionRef = firestore.collection('collections');
+//     dispatch(fetchCollectionsStart());
 
-    collectionRef
-      .get()
-      .then((snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch((error) => {
-        dispatch(fetchCollectionsFailure(error.message));
-      });
-  };
-};
+//     collectionRef
+//       .get()
+//       .then((snapshot) => {
+//         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//         dispatch(fetchCollectionsSuccess(collectionsMap));
+//       })
+//       .catch((error) => {
+//         dispatch(fetchCollectionsFailure(error.message));
+//       });
+//   };
+// };
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,32 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from './shop.actions';
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
