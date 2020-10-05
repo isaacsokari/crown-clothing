32281de5ec57c4a4b3e86330c5f4d011b3e306f2
Isaac Sokari <isaacsokari@gmail.com>
2020-10-05 22:05:14 +0100

    fixed collection page responsiveness on small screens

--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -14,8 +14,14 @@ export const CollectionTitle = styled.h2`
 export const CollectionItemsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
+  gap: 10px;
+
   & > div {
     margin-bottom: 30px;
+  }
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem 1rem;
   }
 `;
