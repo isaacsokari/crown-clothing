f3f538fd5faa6081badba7dab48197d312d53361
Isaac Sokari <isaacsokari@gmail.com>
2020-10-08 00:07:45 +0100

    added initial themes to the app

---
+++ client/src/themes/dark.js
@@ -0,0 +1,12 @@
+const darkTheme = {
+  colors: {
+    background: '#040404',
+    textColor: 'white',
+    buttonColor: '#040404',
+    buttonBackground: 'white',
+    buttonHoverBackground: '#040404',
+    buttonHoverColor: 'white'
+  }
+}
+
+export default darkTheme;
---
+++ client/src/themes/light.js
@@ -0,0 +1,12 @@
+const lightTheme = {
+  colors: {
+    background: 'white',
+    textColor: 'black',
+    buttonColor: 'white',
+    buttonBackground: 'black',
+    buttonHoverBackground: 'white',
+    buttonHoverColor: 'black',
+  },
+};
+
+export default lightTheme;
