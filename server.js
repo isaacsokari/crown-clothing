11d3a207bb0ed797cb84986f98c585b0bf53c41c
Isaac Sokari <isaacsokari@gmail.com>
2020-10-16 11:23:37 +0100

    corrected cors import

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
